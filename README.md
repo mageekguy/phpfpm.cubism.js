63da6c09f7f2c71684fb25ad5dcc0ef65e85c7c9
Frédéric Hardy <frederic.hardy@mageekbox.net>
2013-07-10 13:14:16 +0200

    First commit.

---
+++ README.md
@@ -0,0 +1,39 @@
+# Phpfpm.cubism.js
+
+## A realtime monitoring for PHP-FPM made with [cubism.js](http://square.github.io/cubism/)
+
+![screenshot](/screenshot.png?raw=true)
+
+To use it, first, put contents of directories `js` and `css` file in a directory.--
+
+Second, in the `head` section of an HTML file, add:
+```
+<script type="text/javascript" src="url/to/directory/js/d3.3.2.4.min.js" charset="utf-8"></script>
+<script type="text/javascript" src="url/to/directory/js/cubism.1.3.0.min.js" charset="utf-8"></script>
+<script type="text/javascript" src="url/to/directory/js/phpfpm.cubism.min.js" charset="utf-8"></script>
+<link rel="stylesheet" href="url/to/directory/css/phpfpm.cubism.css" type="text/css" media="screen" />
+```
+
+Third, in the `body` section of your HTML file, add:
+```
+<div id="phpFpmMonitoring"></div>
+<script type="text/javascript">cubism.phpfpm.monitoring('http://url/to/php/fpm/status', 1e3, 960).display('#phpFpmMonitoring');</script>
+```
+
+Moreover, you must enable PHP-FPM status page on your web server.  
+To do that, just give the value `/status` or whatever you want to the directive `pm.status_page` in your PHP-FPM configuration file.--
+Do not omit to restart PHP-FPM!  
+
+At last, you should allow acces to your IP to `/status` in your web server.  
+With [nginx](http://wiki.nginx.org/Main), add in its configuration file:
+```
+location /status {
+    fastcgi_pass 127.0.0.1:9000; # Or use an UNIX socket
+    fastcgi_index index.php;
+    fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include fastcgi_params;
+    allow 127.0.0.1; # Allow your IP here
+    deny all;
+}
+```
+Do not omit to restart [nginx](http://wiki.nginx.org/Main)!
---
+++ js/phpfpm.cubism.js
@@ -0,0 +1,237 @@
+;(function(alias) {
+'use strict';
+
+var phpfpm = alias.phpfpm = {}
+
+phpfpm.url = function(url) {
+	this.url = url;
+
+	this.addQueryString = function(name, value) {
+		var queryString = new RegExp("([?|&])" + name + "=.*?(&|#|$)(.*)", "gi");
+
+		if (queryString.test(this.url)) {
+			this.url = this.url.replace(queryString, '$1' + name + (!value ? '' : '=' + value) + '$2$3');
+		} else {
+			var url = this.url.split('#');
+
+			this.url = url[0] + (this.url.indexOf('?') !== -1 ? '&' : '?') + name;
+			
+			if (value) {
+				this.url += '=' + value;
+			}
+
+			if (url[1]) {
+				this.url += '#' + url[1];
+			}
+		}
+
+		return this;
+	};
+
+	this.toString = function() {
+		return this.url;
+	}
+
+	return this;
+};
+
+phpfpm.status = function(url, delay) {
+	this.url = phpfpm.url(url).addQueryString('json').toString();
+
+	this.delay = delay;
+	this.data =  {};
+	this.previousData = {};
+	this.lastFetch = +(new Date());
+
+	this.fetch = function(callback) {
+		this.lastFetch = +(new Date());
+
+		var status = this;
+
+		d3.json(this.url, function(data) {
+				status.previousData = status.data;
+				status.data = data;
+
+				if (callback) {
+					callback(status);
+				}
+			}
+		);
+
+		return this;
+	}
+
+	this.get = function() {
+		var now = +(new Date());
+
+		if ((this.lastFetch + this.delay) < now) {
+			this.fetch();
+		}
+
+		return this.data;
+	}
+
+	this.getPrevious = function() {
+		return this.previousData;
+	}
+
+	return this;
+};
+
+phpfpm.monitoring = function(url, step, size) {
+	this.context = cubism.context()
+		.serverDelay(0)
+		.clientDelay(0)
+		.step(step)
+		.size(size)
+	;
+
+	this.context
+		.on("focus", function(i) {
+				d3.selectAll(".value").style("right", i == null ? null : size - i + "px");
+			}
+		)
+	;
+
+	this.start = +(new Date());
+
+	this.status = phpfpm.status(url, step);
+
+	this.getMetric = function(name, metricName) {
+		var monitoring = this;
+
+		return this.context.metric(function(start, stop, step, callback) {
+				var values = [];
+
+				start = +start;
+
+				if (start >= monitoring.start) {
+					stop = +stop;
+
+					while (start < stop) {
+						start += step;
+
+						var value = monitoring.status.get()[name];
+
+						if (value) {
+							values.push(value);
+						}
+					}
+				}
+
+				callback(null, values);
+			},
+			(metricName ? metricName : name)
+		);
+	};
+
+	this.getDiffMetric = function(name, metricName) {
+		var monitoring = this;
+
+		return this.context.metric(function(start, stop, step, callback) {
+				var values = [];
+
+				start = +start;
+
+				if (start >= monitoring.start) {
+					stop = +stop;
+
+					while (start < stop) {
+						start += step;
+
+						var value = monitoring.status.get()[name] - monitoring.status.getPrevious()[name];
+
+						if (value) {
+							values.push(value);
+						}
+					}
+				}
+
+				callback(null, values);
+			},
+			metricName
+		);
+	};
+
+	this.getData = function() {
+		return [
+			this.getMetric('accepted conn', 'total conn'),
+			this.getDiffMetric('accepted conn', 'new conn'),
+			this.getMetric('active processes'),
+			this.getDiffMetric('active processes', '~ active processes'),
+			this.getMetric('idle processes'),
+			this.getDiffMetric('idle processes', '~ idle processes'),
+			this.getMetric('total processes'),
+			this.getDiffMetric('total processes', '~ total processes'),
+			this.getMetric('max active processes'),
+			this.getMetric('max children reached'),
+			this.getMetric('listen queue'),
+			this.getDiffMetric('listen queue', '~ listen queue'),
+			this.getMetric('max listen queue')
+		];
+	};
+
+	this.display = function(target, cssClass) {
+		this.start = +(new Date());
+
+		target = d3.select(target);
+
+		if (target) {
+			var phpfpmCssClass = 'phpfpm';
+
+			if (cssClass) {
+				phpfpmCssClass += ' ' + cssClass;
+			}
+
+			target.attr('class', phpfpmCssClass);
+
+			var title = target
+				.append('h1')
+				.attr('class', 'title')
+			;
+
+			this.status.fetch(function(status) {
+					title.text('Pool ' + status.data['pool'] + ' (' + status.data['process manager'] + ') started since ' + new Date(status.data['start time'] * 1000));
+				}
+			);
+
+			var graphe = target
+				.append('div')
+				.attr('class', 'graphe')
+			;
+
+			var monitoring = this;
+
+			graphe
+				.call(function(target) {
+						graphe.append('div')
+							.attr('class', 'axis')
+							.call(monitoring.context.axis().orient('top'))
+						;
+
+						graphe.selectAll('.horizon')
+						  .data(monitoring.getData())
+							 .enter()
+								.append('div')
+									.attr('class', function(d) { return 'horizon ' + d.toString().replace(/\s+/g, ''); })
+									.call(function(horizon) {
+											var horizon = monitoring.context.horizon().format(d3.format('d')).colors([ '#0d58f1', '#568bf5', '#9fbdfa', '#e8effe', '#feefe8', '#fabd9f', '#f58b56', '#f1580d' ])(horizon);
+
+											return horizon;
+										}
+									)
+						;
+
+						graphe.append('div')
+							.attr('class', 'rule')
+							.call(monitoring.context.rule())
+						;
+					}
+				)
+			;
+		}
+	}
+
+	return this;
+};
+})(cubism);
